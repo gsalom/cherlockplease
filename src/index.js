4dae43b5692bc9452a806963415ce3b6211a9dcb
gsalom <guillemsalom@gmail.com>
2024-04-11 20:46:16 +0200

    connectant a bd

--- src/index.js
+++ src/index.js
@@ -2,9 +2,25 @@ import express from "express";
 import morgan from "morgan";
 import { join, dirname } from "path";
 import { fileURLToPath } from "url";
-
+ 
 // Routes
 import indexRoutes from "./routes/index.js";
+
+
+import mysql from "mysql";
+
+var con = mysql.createConnection({
+  host: "localhost",
+  user: "root",
+  password: "diouxx",
+  database: "cherlock"
+});
+
+con.connect(function(err) {
+  if (err) throw err;
+     
+});
+
 
 // Initialize express
 const app = express();
@@ -27,3 +43,17 @@ app.use(express.static(join(__dirname, "public")));
 // listening the Server
 app.listen(app.get("port"));
 console.log("Server on port", app.get("port"));
+
+app.get('/prova', (req, res) => {
+  
+    // Fetch users from the database
+  con.query('SELECT * FROM professorat', (error, results) => {
+      if (error) {
+          console.error('Error fetching users from the database: ' + error.stack);
+          return res.status(500).json({ error: 'Failed to fetch users' });
+    }
+
+    // Send the fetched data as a response
+     res.json(results);
+  });
+});
