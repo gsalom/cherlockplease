abf385d7187c1edf01f73131fe04b97449e10901
gsalom <guillemsalom@gmail.com>
2024-04-16 12:29:20 +0200

    commit

--- src/index.js
+++ src/index.js
@@ -46,14 +46,66 @@ console.log("Server on port", app.get("port"));
 
 app.get('/prova', (req, res) => {
   
-    // Fetch users from the database
+    // Fetch profes from the database
   con.query('SELECT * FROM professorat', (error, results) => {
       if (error) {
-          console.error('Error fetching users from the database: ' + error.stack);
-          return res.status(500).json({ error: 'Failed to fetch users' });
+          console.error('Error fetching profes from the database: ' + error.stack);
+          return res.status(500).json({ error: 'Failed to fetch professorat' });
     }
 
     // Send the fetched data as a response
      res.json(results);
   });
+});
+
+app.get('/carretons2', async (req, res) => {
+  await con.query('SELECT * FROM carretons', (error, results) => {
+    if (error) {
+        console.error('Error fetching carretons from the database: ' + error.stack);
+        return res.status(500).json({ error: 'Failed to fetch carretons' });
+  }
+  // Send the fetched data as a response
+  const html = `
+        <!DOCTYPE html>
+        <html lang="es">
+        <head>
+            <meta charset="UTF-8">
+            <meta name="viewport" content="width=device-width, initial-scale=1.0">
+            <title>Carretons</title>
+            <style>
+                table {
+                    border-collapse: collapse;
+                    width: 100%;
+                }
+
+                th, td {
+                    text-align: left;
+                    padding: 8px;
+                    border: 1px solid #ddd;
+                }
+            </style>
+        </head>
+        <body>
+            <h1>Carretons</h1>
+
+            <table>
+                <thead>
+                    <tr>
+                        ${fields.map(field => `<th>${field.name}</th>`).join('')}
+                    </tr>
+                </thead>
+                <tbody>
+                    ${rows.map(row => `
+                        <tr>
+                            ${row.map(value => `<td>${value}</td>`).join('')}
+                        </tr>
+                    `).join('')}
+                </tbody>
+            </table>
+        </body>
+        </html>
+    `;
+
+    res.send(html);
+});
 });
