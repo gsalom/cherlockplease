7a87f76789e9a6edb96d1de264f8ad60c1e2b17f
gsalom <guillemsalom@gmail.com>
2024-04-17 20:51:36 +0200

    menus i pantalles backend

--- src/routes/index.js
+++ src/routes/index.js
@@ -20,6 +20,10 @@ router.get("/", (req, res) => {
   res.render("index", { title: "CherLock Please" });
 });
 
+router.get("/backend", (req, res) => {
+  res.render("backend", { title: "CherLock Back End" });
+});
+
 router.get("/about", (req, res) => {
   res.render("about", { title: "Sobre CherLock Please" });
 });
@@ -52,6 +56,54 @@ con.query('SELECT * FROM professorat', (error, results) => {
 });
 });
 
+router.get('/departaments', (req, res) => {
+  // Fetch departaments from the database
+con.query('SELECT * FROM departaments', (error, results) => {
+    if (error) {
+        console.error('Error fetching professorat from the database: ' + error.stack);
+        return res.status(500).json({ error: 'Failed to fetch professorat' });
+  }
+  // Send the fetched data as a response
+  res.render("departaments", {  title: "Departaments", data: results });
+});
+});
+
+router.get('/cicles', (req, res) => {
+  // Fetch cicles from the database
+con.query('SELECT * FROM cicles', (error, results) => {
+    if (error) {
+        console.error('Error fetching cicles from the database: ' + error.stack);
+        return res.status(500).json({ error: 'Failed to fetch cicles' });
+  }
+  // Send the fetched data as a response
+  res.render("cicles", {  title: "Cicles", data: results });
+});
+});
+
+router.get('/aules', (req, res) => {
+  // Fetch aules from the database
+con.query('SELECT * FROM aules', (error, results) => {
+    if (error) {
+        console.error('Error fetching aules from the database: ' + error.stack);
+        return res.status(500).json({ error: 'Failed to fetch aules' });
+  }
+  // Send the fetched data as a response
+  res.render("aules", {  title: "Aules", data: results });
+});
+});
+router.get('/grups', (req, res) => {
+  // Fetch grups from the database
+con.query('SELECT * FROM grups', (error, results) => {
+    if (error) {
+        console.error('Error fetching grups from the database: ' + error.stack);
+        return res.status(500).json({ error: 'Failed to fetch grups' });
+  }
+  // Send the fetched data as a response
+  res.render("grups", {  title: "Grups", data: results });
+});
+});
+
+
 router.get('/horaris', (req, res) => {
   // Fetch professorat from the database
 con.query('SELECT c.nom as cicle, a.nom as aula, concat(p.llin2," ",p.llin1,", ",p.nom) as profe, h.dia, h.hora, h.realitzada, h.tipus FROM cherlock.horaris h, cherlock.professorat p, cherlock.cicles c, cherlock.aules a  where h.id_cicle=c.codi and h.id_prof=p.codi and h.id_aula=a.codi', (error, results) => {
@@ -67,13 +119,13 @@ con.query('SELECT c.nom as cicle, a.nom as aula, concat(p.llin2," ",p.llin1,", "
 
 router.get('/revisions', (req, res) => {
   // Fetch revisions from the database
-con.query('SELECT date_format(r.data_rev, '%d %m %y') as dia, r.hora_rev, concat(p.llin2," ",p.llin1,", ",p.nom) as profe, r.aula, c.nom as carreto, r.estat, r.comentaris FROM cherlock.revisions r, cherlock.professorat p, cherlock.carretons c where r.email=p.email and r.id_aula=c.codi_aula', (error, results) => {
+con.query('SELECT date_format(r.data_rev, "%d/%m/%y") as dia, r.hora_rev, concat(p.llin2," ",p.llin1,", ",p.nom) as profe, r.aula, c.nom as carreto, r.estat, r.comentaris FROM cherlock.revisions r, cherlock.professorat p, cherlock.carretons c where r.email=p.email and r.id_aula=c.codi_aula', (error, results) => {
     if (error) {
         console.error('Error fetching revisions from the database: ' + error.stack);
         return res.status(500).json({ error: 'Failed to fetch revisions' });
   }
   // Send the fetched data as a response
-  res.render("revisions", {  title: "revisions", data: results });
+  res.render("revisions", {  title: "Revisions", data: results });
 });
 });
 
