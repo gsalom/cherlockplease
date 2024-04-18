58b79d35e780c31ed9709516c3b762f480848422
gsalom <guillemsalom@gmail.com>
2024-04-18 11:00:33 +0200

    forms i operacions db

---
+++ src/dboperacions.js
@@ -0,0 +1,61 @@
+var mysql = require('mysql');
+
+var con = mysql.createConnection({
+  host: "localhost",
+  user: "myusername",
+  password: "mypassword",
+  database: "mydb"
+});
+
+//Insert
+
+con.connect(function(err) {
+  if (err) throw err;
+  console.log("Connected!");
+  //Insert a record in the "customers" table:
+  var sql = "INSERT INTO customers (name, address) VALUES ('Company Inc', 'Highway 37')";
+  con.query(sql, function (err, result) {
+    if (err) throw err;
+    console.log("Number of records inserted: " + result.affectedRows);
+  });
+});
+
+//Delete
+
+con.connect(function(err) {
+    if (err) throw err;
+    //Delete all customers with the address "Mountain 21":
+    var sql = "DELETE FROM customers WHERE address = 'Mountain 21'";
+    con.query(sql, function (err, result) {
+      if (err) throw err;
+      console.log("Number of records deleted: " + result.affectedRows);
+    });
+  });
+  
+//Update
+
+con.connect(function(err) {
+    if (err) throw err;
+    //Update the address field:
+    var sql = "UPDATE customers SET address = 'Canyon 123' WHERE address = 'Valley 345'";
+    con.query(sql, function (err, result) {
+      if (err) throw err;
+      console.log(result.affectedRows + " record(s) updated");
+    });
+  });
+  
+
+/*
+{
+    fieldCount: 0,
+    affectedRows: 14,
+    insertId: 0,
+    serverStatus: 2,
+    warningCount: 0,
+    message: '\'Records:14  Duplicated: 0  Warnings: 0',
+    protocol41: true,
+    changedRows: 0
+  }
+
+  */
+
