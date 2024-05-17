086e0c1ffd826a82b09c55f1c900c463f5ce604c
gsalom <guillemsalom@gmail.com>
2024-05-17 18:58:45 +0200

    2n gràfica

--- src/databd/sqls/dashboard.sql
+++ src/databd/sqls/dashboard.sql
@@ -2,3 +2,9 @@ SELECT * FROM cherlock.revisionsnofetes;
 SELECT SUM(consumo_combustible.importe) as imp, MONTH(consumo_combustible.fecha) as mes FROM consumo_combustible WHERE YEAR(consumo_combustible.fecha)=2018 GROUP BY consumo_combustible.fecha;
 
 SELECT cherlock.revisionsnofetes.professorat, COUNT(cherlock.revisionsnofetes.email) as vegades FROM cherlock.revisionsnofetes WHERE YEAR(cherlock.revisionsnofetes.dia)=2024 GROUP BY cherlock.revisionsnofetes.professorat order by vegades;
+
+SELECT * FROM cherlock.revisions;
+SELECT cherlock.revisions.estat, COUNT(cherlock.revisions.estat) FROM cherlock.revisions WHERE YEAR(cherlock.revisions.data_rev)=2024 GROUP BY cherlock.revisions.estat;
+SELECT cherlock.revisions.estat, COUNT(cherlock.revisions.estat) FROM cherlock.revisions GROUP BY cherlock.revisions.estat;
+
+SELECT COUNT(*) FROM cherlock.horaris where tipus=1;
--- src/routes/index.js
+++ src/routes/index.js
@@ -39,31 +39,41 @@ router.get("/dashboard", (req, res) => {
       var labels1=[];
       var data1=[];  
       var sql = "SELECT cherlock.revisionsnofetes.professorat, COUNT(cherlock.revisionsnofetes.email) as vegades FROM cherlock.revisionsnofetes WHERE YEAR(cherlock.revisionsnofetes.dia)=2024 GROUP BY cherlock.revisionsnofetes.professorat order by vegades;";
+      var labels2=[];
+      var data2=[];  
+      var sql2 = "SELECT cherlock.revisions.estat, COUNT(cherlock.revisions.estat) as total FROM cherlock.revisions  WHERE (cherlock.revisions.data_rev > '2024-04-14' AND cherlock.revisions.data_rev  < '2024-04-18') GROUP BY cherlock.revisions.estat "+
+      " UNION SELECT cherlock.revisions.estat, COUNT(cherlock.revisions.estat) FROM cherlock.revisions  WHERE (cherlock.revisions.data_rev > '2024-04-14' AND cherlock.revisions.data_rev  < '2024-04-18')" +
+      " UNION SELECT cherlock.horaris.tipus, COUNT(*) FROM cherlock.horaris where tipus=1 GROUP BY cherlock.horaris.tipus;";
+  
   if (sql) {
     con.query(sql, function (err, result) {
       if (err) throw err;
-      console.log(result[0].professorat);
-      console.log(result[0].vegades);
-      
-
-      
+      // console.log(result[0].professorat);
+      // console.log(result[0].vegades);
       for (var i=0; i < result.length ; ++i){
               labels1.push(result[i].professorat+"#");
               data1.push(result[i].vegades);
       }
-
-    res.render("dashboard", {
-      title: "Panel d'Estat",
-      labels1: labels1,
-      data1: data1,
-      datagraf2: [10, 5, 4, 1],
-      datagraf3: [10, 5, 4, 1],
-      datagraf4: [10, 5, 4, 1],
-    });
-
     })
-  }
-});
+    con.query(sql2, function (err, result) {
+      if (err) throw err;
+      // console.log(result[0].estat);
+      // console.log(result[0].total);
+      for (var i=0; i < result.length ; ++i){
+              labels2.push(result[i].estat+"#");
+              data2.push(result[i].total);
+      }
+      res.render("dashboard", {
+        title: "Panel d'Estat",
+        labels1: labels1,
+        data1: data1,
+        labels2: labels2,
+        data2: data2,
+        datagraf3: [10, 5, 4, 1],
+        datagraf4: [10, 5, 4, 1],
+      });
+    })   
+}});
 
 router.get("/about", (req, res) => {
   res.render("about", {
