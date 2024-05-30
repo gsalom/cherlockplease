0646f54ed96988f362064aa942597bc03e417cff
gsalom <guillemsalom@gmail.com>
2024-05-30 20:39:00 +0200

    usuaris ....

--- src/routes/index.js
+++ src/routes/index.js
@@ -23,9 +23,15 @@ con.connect(function (err) {
 
 const router = Router();
 
-router.get("/", (req, res) => {
+router.get("/home", (req, res) => {
   res.render("index", {
     title: "CherLock Please"
+  });
+});
+
+router.get("/", (req, res) => {
+  res.render("login", {
+    title: "CherLock login"
   });
 });
 
@@ -690,6 +696,54 @@ router.get('/prova', (req, res) => {
     title: "Menu"
   });
 });
+
+
+
+// http://localhost:3000/auth
+router.post('/auth', function(request, response) {
+	// Capture the input fields
+	let username = request.body.username;
+	let password = request.body.password;
+	// Ensure the input fields exists and are not empty
+	if (username && password) {
+		// Execute SQL query that'll select the account from the database based on the specified username and password
+		connection.query('SELECT * FROM usuaris WHERE username = ? AND password = ?', [username, password], function(error, results, fields) {
+			// If there is an issue with the query, output the error
+			if (error) throw error;
+			// If the account exists
+			if (results.length > 0) {
+				// Authenticate the user
+				request.session.loggedin = true;
+				request.session.username = username;
+				// Redirect to home page
+				response.redirect('/home');
+			} else {
+				response.send('Usuario y/o Contraseña Incorrecta');
+			}			
+			response.end();
+		});
+	} else {
+		response.send('Por favor ingresa Usuario y Contraseña!');
+		response.end();
+	}
+});
+
+// http://localhost:3000/home
+// app.get('/home', function(request, response) {
+// 	// If the user is loggedin
+// 	if (request.session.loggedin) {
+// 		// Output username
+// 		response.send('Te has logueado satisfactoriamente: ' + request.session.username + '!');
+// 		// response.render("index", {
+// 		// 	title: "CherLock Please"
+// 		//   });
+// 	} else {
+// 		// Not logged in
+// 		response.send('¡Inicia sesión para ver esta página!');
+// 	}
+// 	response.end();
+// });
+
 
 
 export default router;
