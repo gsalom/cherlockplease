8e8f9d71619f8a27b1e00a04211ba93259630c50
gsalom <guillemsalom@gmail.com>
2024-09-05 12:59:56 +0200

    nou curs

--- src/databd/sqls/ordinadors.sql
+++ src/databd/sqls/ordinadors.sql
@@ -1,28 +1,14 @@
-select * from ordinadors;
+SELECT * FROM cherlock.ordinadors;
 
-I
-NSERT INTO `cherlock`.`ordinadors` (`nom`,`carreto`,`caracteristiques`,`estat`,`assignacio`) VALUES ('ARIEL22',8,'adasdasa ',1,'EFC44F17B11FF3E705974311ADB5C48B');
+SELECT c.nom as carreto, o.nom, concat(a.llin1,' ',a.llin2,', ',a.nom) as assignat,
+ o.estat, o.comentaris 
+ FROM cherlock.ordinadors o, cherlock.carretons c, 
+	(select a.codi, a.llin1, a.llin2, a.nom FROM cherlock.alumnat a
+     UNION ALL
+     select p.codi, p.llin1, p.llin2, p.nom FROM cherlock.professorat p) a
+ where o.carreto=c.id_car and o.assignacio=a.codi and c.id_car=10;
+ 
 
-
-INSERT INTO `cherlock`.`ordinadors` (`nom`,`carreto`,`caracteristiques`,`estat`,`assignacio`,`comentaris`) VALUES ('ARIEL24',8,'adasdasa ',1,null,'ROBAT');
-
-UPDATE `cherlock`.`ordinadors` SET `estat` = 3 WHERE `comentaris` = 'RETIRAT';
-
-INSERT INTO `cherlock`.`ordinadors` (`nom`,`carreto`,`caracteristiques`,`estat`,`assignacio`) VALUES ('CARONTE02',9,'adasdasa ',1,'MARTÍ ENSEÑAT, GUILLEM');
-
-INSERT INTO `cherlock`.`ordinadors` (`nom`,`carreto`,`caracteristiques`,`estat`,`assignacio`) VALUES ('CARONTE04',9,'adasdasa ',1,'MASCARO RUIZ, PEDRO');
-INSERT INTO `cherlock`.`ordinadors` (`nom`,`carreto`,`caracteristiques`,`estat`,`assignacio`) VALUES ('CARONTE05',9,'adasdasa ',1,'MENACHO COPA, CAMILA');
-INSERT INTO `cherlock`.`ordinadors` (`nom`,`carreto`,`caracteristiques`,`estat`,`assignacio`) VALUES ('CARONTE06',9,'adasdasa ',1,'MOLINA CIFRE, ALEJANDRO');
-INSERT INTO `cherlock`.`ordinadors` (`nom`,`carreto`,`caracteristiques`,`estat`,`assignacio`) VALUES ('CARONTE07',9,'adasdasa ',1,'MORENO JARA, IAN');
-INSERT INTO `cherlock`.`ordinadors` (`nom`,`carreto`,`caracteristiques`,`estat`,`assignacio`) VALUES ('CARONTE08',9,'adasdasa ',1,'QUIROGA ÁLVAREZ, PABLO AGUSTÍN');
-INSERT INTO `cherlock`.`ordinadors` (`nom`,`carreto`,`caracteristiques`,`estat`,`assignacio`) VALUES ('CARONTE09',9,'adasdasa ',1,'RAIMOV, AYAN');
-INSERT INTO `cherlock`.`ordinadors` (`nom`,`carreto`,`caracteristiques`,`estat`,`assignacio`) VALUES ('CARONTE11',9,'adasdasa ',1,'SANCHEZ SANCHEZ, JULIÀ');
-INSERT INTO `cherlock`.`ordinadors` (`nom`,`carreto`,`caracteristiques`,`estat`,`assignacio`) VALUES ('CARONTE12',9,'adasdasa ',1,'SZCZEPANSKI, PHILIP');
-INSERT INTO `cherlock`.`ordinadors` (`nom`,`carreto`,`caracteristiques`,`estat`,`assignacio`) VALUES ('CARONTE13',9,'adasdasa ',1,'TAIBI, MOUNIR');
-INSERT INTO `cherlock`.`ordinadors` (`nom`,`carreto`,`caracteristiques`,`estat`,`assignacio`) VALUES ('CARONTE14',9,'adasdasa ',1,'TRAORE DIABY, ALMAMY');
-INSERT INTO `cherlock`.`ordinadors` (`nom`,`carreto`,`caracteristiques`,`estat`,`assignacio`) VALUES ('CARONTE16',9,'adasdasa ',1,'VALDEZ CONDORI, GERY');
-INSERT INTO `cherlock`.`ordinadors` (`nom`,`carreto`,`caracteristiques`,`estat`,`assignacio`) VALUES ('CARONTE17',9,'adasdasa ',1,'VERA MENCHACA, LUIS GUILLERMO');
-INSERT INTO `cherlock`.`ordinadors` (`nom`,`carreto`,`caracteristiques`,`estat`,`assignacio`) VALUES ('CARONTE19',9,'adasdasa ',1,'ZUÑIGA OLAVE, CAMILO');
-INSERT INTO `cherlock`.`ordinadors` (`nom`,`carreto`,`caracteristiques`,`estat`,`assignacio`) VALUES ('CARONTE20',9,'adasdasa ',1,'TUMBACO URGILES, JOSEPH ISAAC');
-INSERT INTO `cherlock`.`ordinadors` (`nom`,`carreto`,`caracteristiques`,`estat`,`assignacio`) VALUES ('CARONTE21',9,'adasdasa ',1,'MACHUCA LOPEZ, NELSON');
-INSERT INTO `cherlock`.`ordinadors` (`nom`,`carreto`,`caracteristiques`,`estat`,`assignacio`) VALUES ('CARONTE23',9,'adasdasa ',1,'WANG, JIE');
+SELECT o.nom, concat(a.llin1,' ',a.llin2,', ',a.nom) as assignat, c.nom as carreto, o.comentaris as estat FROM cherlock.ordinadors o,  cherlock.carretons c, (select a.codi, a.llin1, a.llin2, a.nom FROM cherlock.alumnat a
+     UNION ALL
+     select p.codi, p.llin1, p.llin2, p.nom FROM cherlock.professorat p) a where o.carreto=c.id_car and o.assignacio=a.codi order by o.nom;
